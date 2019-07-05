7382defc6dbe7e1eaa17cc1e4d2afadf892b25c0
takumi34 <02rryoi@gmail.com>
2019-07-05 19:53:48 +0900

    add plymouthd

--- plymouth_change.sh
+++ plymouth_change.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-echo -n 'Plymouthのpathを入力'
+echo -n 'Plymouthのディレクトリ名を入力'
+read path1
+echo -n 'plymouthのファイル名を入力'
+read path2
 
-read path
+sudo cp -rf $path1 /usr/share/plymouth/themes/$path1
 
-sudo cp -r $path /usr/share/plymouth/themes/$path
-
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/$path 100
+sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/$path1/$path2 100
 
 sudo update-alternatives --config default.plymouth
 
 sudo update-initramfs -u
  
+sudo plymouthd ; sudo plymouth --show-splash ; for ((I=0; I<10; I++)); do sleep 1 ; sudo plymouth --update=test$I ; done ; sudo plymouth --quit
